b9d025e5a4006c617a5fd0bf79211b94820f3b47
Crowdbotics <team@crowdbotics.com>
2021-09-28 02:02:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "royal_wave_30911",
+  url: "https://royal_wave_30911.botics.co",
+  api: "https://royal_wave_30911.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
